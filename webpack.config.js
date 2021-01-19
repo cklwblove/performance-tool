6241cd52043ff762a6f1e1992ebce773f4fdb805
文博 <lwbhtml@163.com>
2021-01-19 14:49:13 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-      performance: './src/index.js'
+      performance: './lib/index.js'
     },
     output: {
         path: path.join(__dirname, 'dist'),
